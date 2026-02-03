0624015bd9e95ed838d669220b69e50ab57083b3
hyojeong <gywjd__gywjd@naver.com>
2026-02-03 19:02:02 +0900

    hospital_support 백엔드 프로젝트 생성, 저장

---
+++ hospital-support/src/main/java/kr/co/seoulit/hospitalsupport/HospitalSupportApplication.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.hospitalsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSupportApplication.class, args);
+    }
+
+}
---
+++ hospital-support/src/test/java/kr/co/seoulit/hospitalsupport/HospitalSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.hospitalsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
