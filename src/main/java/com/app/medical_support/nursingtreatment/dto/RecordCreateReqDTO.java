1e36adcb2b6ef7e8865ea6a38045db70d6a18743
hyojeong <gywjd__gywjd@naver.com>
2026-04-14 21:08:11 +0900

    0414

--- src/main/java/com/app/medical_support/nursingtreatment/dto/RecordCreateReqDTO.java
+++ src/main/java/com/app/medical_support/nursingtreatment/dto/RecordCreateReqDTO.java
@@ -1,8 +1,10 @@
 package com.app.medical_support.nursingtreatment.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @Setter
 public class RecordCreateReqDTO {
